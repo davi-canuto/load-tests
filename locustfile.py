77e51d121ee453ebae557f43fe423c874afde4a3
davi-canuto <davicanutogregorio@gmail.com>
2024-08-26 21:23:03 -0300

    set wait time

--- locustfile.py
+++ locustfile.py
@@ -2,7 +2,7 @@ from locust import HttpUser, task, constant
 
 class HelloWorldUser(HttpUser):
     host = "http://localhost:8000" 
-    wait_time = constant(0)
+    wait_time = constant(1)
     
     @task
     def hello_world(self):
