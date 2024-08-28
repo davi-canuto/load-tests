e923af6346199ed2a3fb5a5689a8c3298025b56d
davi-canuto <davicanutogregorio@gmail.com>
2024-08-28 19:55:40 -0300

    improves flask app and locustfile

--- locustfile.py
+++ locustfile.py
@@ -1,9 +1,11 @@
-from locust import HttpUser, task, constant
+from locust import HttpUser, task, between
 
-class HelloWorldUser(HttpUser):
+class SimpleLoadTestUser(HttpUser):
     host = "http://localhost:8000" 
-    wait_time = constant(1)
-    
-    @task
-    def hello_world(self):
-        self.client.get("/")
+    wait_time = between(1, 2)
+
+    @task(1)
+    def test_list_items(self):
+
+    @task(2)
+    def test_create_item(self):
